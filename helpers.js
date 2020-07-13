bedba84964714a229b6c267e9d32ab726540417e
Max Zawisa <maxrzaw@umich.edu>
2020-07-13 16:25:19 -0400

    Working on adding device detection

--- helpers.js
+++ helpers.js
@@ -1,7 +1,8 @@
 // Some helper functions an constants
 
-export const BASE_URL = "http://localhost:8000/api/v1/"
-//'http://192.168.1.15:8000/api/v1/'
+// export const BASE_URL = "http://localhost:8000/api/v1/"
+export const BASE_URL = "http://10.0.0.234:8000/api/v1/"
+
 
 export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
