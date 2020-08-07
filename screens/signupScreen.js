138e0f2dfa237343e1663dfe371cf58d22ea4d16
Max Zawisa <maxrzaw@umich.edu>
2020-08-07 17:58:01 -0400

    Working on the sign up screen.
    Just got started.

--- screens/signupScreen.js
+++ screens/signupScreen.js
@@ -1,8 +1,34 @@
 import * as React from 'react';
+import { useState, useContext } from 'react';
 import { Text, View, Button } from 'react-native';
+
+/*
+Things I need:
+Required:
+ - email
+ - username
+ - password1
+ - password2
+ - year
+Optional:
+ - first_name (probably want to make this required since I show it)
+ - last_name
+ - bio
+*/
+
 
 
 export default function SignUpScreen({ navigation }) {
+  const [state, setState] = useState({
+    email: '',
+    username: '',
+    password1: '',
+    password2: '',
+    year: '',
+    first_name: '',
+    last_name: '',
+    bio: '',
+  });
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Sign Up!</Text>
